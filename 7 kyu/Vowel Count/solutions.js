54bfd5f05f9e307f4d9ceb00584bc0fdacd2308f
Yimiuxi <snowminger@hotmail.com>
2019-05-28 22:21:10 +0800

    vowel count solutions

--- 7 kyu/Vowel Count/solutions.js
+++ 7 kyu/Vowel Count/solutions.js
@@ -1,1 +1,15 @@
-明天写
+/*
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, and u as vowels for this Kata.
+
+The input string will only consist of lower case letters and/or spaces.
+*/
+
+
+
+function getCount(str) {
+  var vowelsCount = 0;
+  var m = str.match(/[aeiou]/gi);
+  return m === null ? 0 : m.length;
+}
